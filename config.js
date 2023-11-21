c9751a19c52b3b51a6a456c1989724e81fb8f314
simon409 <addarm409@gmail.com>
2023-11-21 16:38:41 +0100

    env var

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCu0Ijc-fVbb5SAk3eIcs07axf8r6ljH5s",
-  authDomain: "communityguard-583fc.firebaseapp.com",
-  databaseURL: "https://communityguard-583fc-default-rtdb.firebaseio.com",
-  projectId: "communityguard-583fc",
-  storageBucket: "communityguard-583fc.appspot.com",
-  messagingSenderId: "1068169364444",
-  appId: "1:1068169364444:web:bdbb5f56afec1545d14519",
+  apiKey: process.env.EXPO_PUBLIC_apikey,
+  authDomain: process.env.EXPO_PUBLIC_authDomain,
+  databaseURL: process.env.EXPO_PUBLIC_databaseURL,
+  projectId: process.env.EXPO_PUBLIC_projectId,
+  storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+  messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+  appId: process.env.EXPO_PUBLIC_appId,
 };
 
 if (!firebase.apps.length) {
